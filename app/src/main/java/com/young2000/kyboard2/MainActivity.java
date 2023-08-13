b16a6db609f4ae58cbb0a66072be8e872f9e3cee
kyle <kyle.jeong@gmail.com>
2023-08-13 16:14:16 +0900

    If the keyboard is not enabled, open the Manage Keyboard Service

--- app/src/main/java/com/young2000/kyboard2/MainActivity.java
+++ app/src/main/java/com/young2000/kyboard2/MainActivity.java
@@ -2,7 +2,14 @@ package com.young2000.kyboard2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InputMethodManager imeManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        List<InputMethodInfo> enabledMethods = imeManager.getEnabledInputMethodList();
+
+        boolean checkPass = false;
+        for (InputMethodInfo method : enabledMethods) {
+            if (method.getPackageName().equals("com.young2000.kyboard2")) {
+                // Your keyboard is enabled
+                Toast.makeText(this, "Kyboard is registered", Toast.LENGTH_LONG).show();;
+                checkPass = true;
+            }
+        }
+        if (!checkPass) {
+            Toast.makeText(this, "Kyboard is not registered", Toast.LENGTH_LONG).show();;
+            Intent enableIntent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+            startActivity(enableIntent);
+        }
+
+        String selectedKeyboardId = Settings.Secure.getString(getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD);
+
+        if (selectedKeyboardId != null && selectedKeyboardId.contains("com.young2000.kyboard2")) {
+            // Your keyboard is currently selected
+            Toast.makeText(this, "Kyboard is selected currently", Toast.LENGTH_LONG).show();;
+        } else {
+            // Your keyboard is not currently selected
+            Toast.makeText(this, "Kyboard is not selected currently", Toast.LENGTH_LONG).show();;
+        }
     }
 }
